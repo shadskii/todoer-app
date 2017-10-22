cabcd7cec041ddd756ff95c5ea29035ece47bb38
jkh416 <jkh416@lehigh.edu>
2017-10-21 22:22:13 -0400

    Changed styling. Added checkboxes

--- src/main/java/freetimelabs/desktop/app/MainScreenController.java
+++ src/main/java/freetimelabs/desktop/app/MainScreenController.java
@@ -1,9 +1,11 @@
 package freetimelabs.desktop.app;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
-
-import java.util.Objects;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.CheckBoxListCell;
+import javafx.util.StringConverter;
 
 public class MainScreenController
 {
@@ -18,23 +20,8 @@ public class MainScreenController
     private TextField description;
 
     @FXML
-    private Button completed;
-
-    @FXML
     public void initialize()
     {
-        itemList.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) ->
-                completed.setText(newVal.completedProperty().get() ? "Undo Complete" : "Complete"));
-        completed.setOnAction(e ->
-        {
-            TodoItemModel selected = itemList.getSelectionModel().getSelectedItem();
-            if (Objects.nonNull(selected))
-            {
-                selected.completedProperty().setValue(!selected.completedProperty().get());
-                completed.setText(selected.completedProperty().get() ? "Undo Complete" : "Complete");
-            }
-        });
-
         addItem.setOnAction(e ->
         {
             String desc = description.getText();
@@ -44,35 +31,20 @@ public class MainScreenController
                 description.clear();
             }
         });
-        itemList.setCellFactory(lv -> new ListCell<TodoItemModel>()
+        itemList.setCellFactory(CheckBoxListCell.forListView(TodoItemModel::completedProperty, new StringConverter<TodoItemModel>()
         {
             @Override
-            protected void updateItem(TodoItemModel itemModel, boolean empty)
+            public String toString(TodoItemModel object)
             {
-                super.updateItem(itemModel, empty);
-                if (empty)
-                {
-                    setGraphic(null);
-                    setText(null);
-                } else
-                {
-                    Label text = new Label(itemModel.descriptionProperty().get());
-                    setGraphic(text);
-                    itemModel.completedProperty().addListener((obs, oldVal, newVal) ->
-                    {
-                        System.out.println(text + " " + newVal);
-                        if (newVal)
-                        {
-                            text.setStyle("-fx-strikethrough: true");
-                        } else
-                        {
-                            text.setStyle("-fx-strikethrough: false");
-                        }
-                    });
-
-                }
+                return object.descriptionProperty().get();
             }
-        });
+
+            @Override
+            public TodoItemModel fromString(String string)
+            {
+                return null;
+            }
+        }));
     }
 
 }
